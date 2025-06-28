27bbd280a98d35fec35f36afca11888fcb4de04d
asuhankakseto <56214248+asuhankakseto@users.noreply.github.com>
2025-06-28 13:54:12 +0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,22 @@
+MAGICEDEN
+➡️ Go to : https://magiceden.io/launchpad
+Monad x Alchemy Mint
+Emmy On Monad Mint
+
+MORKIE
+➡️ Go to : https://morkie.xyz/monad
+➖️ Complete Some Quest
+➖️ Mint Morkie ID NFT
+➖️ Check Every Day / Week, If have free NFT you can mint
+➖️ Done
+
+NADS DOMAIN
+➡️ Go to : https://app.nad.domains
+
+
+NFTS2ME
+➡️ Go to : https://nfts2me.com/create/edition/
+
+
+MONADVERSE
+➡️ Go to : https://www.monadverse.land/mint
